b51053832df1d123fde6eb0c259b49650dfad711
Aleksandr Usov <wrcomb@gmail.com>
2020-04-23 00:45:31 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# fossa
+
+Migration plan on Fossa
+
+## Step 1 - install ansible
+
+`ansible.sh` install ansible into pyenv-virtualenv. Just run it `./ansible.sh`
+
+## Step 2 - apply the ansible playbook
---
+++ ansible.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+set -o errexit -o nounset -o xtrace
+
+readonly custom_bashrc="$HOME/.bashrc_$USER"
+readonly python_version="3.8.2"
+readonly venv="ansible"
+
+echo "Installing git"
+sudo apt update
+sudo apt install -y git
+
+cd
+
+if [[ -d ~/.pyenv ]]; then
+  cd .pyenv
+  git pull
+  cd
+else
+  git clone https://github.com/pyenv/pyenv.git .pyenv
+fi
+
+if [[ ! -f ${custom_bashrc} ]]; then
+  echo '# reading custom .bashrc' >> .bashrc
+  echo ". ${custom_bashrc}" >> .bashrc
+fi
+
+touch ${custom_bashrc}
+chmod 644 ${custom_bashrc}
+
+echo '# pyenv activating'
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ${custom_bashrc}
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ${custom_bashrc}
+echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ${custom_bashrc}
+
+echo "Install dependencies"
+sudo apt -y install build-essential libbz2-dev libffi-dev libreadline-dev libsqlite3-dev libssl-dev zlib1g-dev
+
+. ${custom_bashrc}
+
+pyenv install ${python_version}
+
+git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
+
+echo '# pyenv-virtualenv activating'
+echo 'eval "$(pyenv virtualenv-init -)"' >> ${custom_bashrc}
+echo 'export PYENV_VIRTUALENV_DISABLE_PROMPT=1' >> ${custom_bashrc}
+
+set +o nounset
+. ${custom_bashrc}
+set -o nounset
+
+pyenv global ${python_version}
+pyenv virtualenv ${venv}
+
+set +o nounset
+pyenv activate ${venv}
+set -o nounset
+
+pip install ansible
+ansible --version
